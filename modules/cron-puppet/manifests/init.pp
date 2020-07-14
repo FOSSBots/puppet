8c0894b6376f9d76b341f52887b5261d3f08cdc0
RhinosF1 <rhinosf1@gmail.com>
2020-07-14 14:41:24 +0100

    Create init.pp

---
+++ modules/cron-puppet/manifests/init.pp
@@ -0,0 +1,17 @@
+class cron-puppet {
+    file { 'post-hook':
+        ensure  => file,
+        path    => '/etc/puppet/.git/hooks/post-merge',
+        source  => 'puppet:///modules/cron-puppet/post-merge',
+        mode    => 0755,
+        owner   => root,
+        group   => root,
+    }
+    cron { 'puppet-apply':
+        ensure  => present,
+        command => "cd /etc/puppet ; /usr/bin/git pull",
+        user    => root,
+        minute  => '*/30',
+        require => File['post-hook'],
+    }
+}
