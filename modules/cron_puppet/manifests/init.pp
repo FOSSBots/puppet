db66bfab8790fecd5836e55859e0c9fbea3b1a80
RhinosF1 <rhinosf1@gmail.com>
2020-07-15 13:10:10 +0100

    Update init.pp

--- modules/cron_puppet/manifests/init.pp
+++ modules/cron_puppet/manifests/init.pp
@@ -1,22 +1,8 @@
 class cron_puppet {
-    file { 'post-hook':
-        ensure  => absent,
-        path    => '/etc/puppet/code/.git/hooks/post-merge',
-        source  => 'puppet:///modules/cron_puppet/post-merge',
-        mode    => '0755',
-        owner   => root,
-        group   => root,
-    }
     cron { 'puppet-pull':
         ensure  => present,
         command => "cd /etc/puppet/code ; /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp",
         user    => root,
         minute  => '*/10',
-    }
-    cron { 'puppet-apply':
-        ensure  => absent,
-        command => "sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp",
-        user    => root,
-        minute  => '*/15',
     }
 }
