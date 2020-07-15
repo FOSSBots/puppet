15fc00b2a3987e714f8a004b789cc08a55baca18
RhinosF1 <rhinosf1@gmail.com>
2020-07-15 11:33:31 +0100

    run puppet every 10 mins under all cases, stop using post-merge

--- modules/cron_puppet/manifests/init.pp
+++ modules/cron_puppet/manifests/init.pp
@@ -1,6 +1,6 @@
 class cron_puppet {
     file { 'post-hook':
-        ensure  => file,
+        ensure  => absent,
         path    => '/etc/puppet/code/.git/hooks/post-merge',
         source  => 'puppet:///modules/cron_puppet/post-merge',
         mode    => '0755',
@@ -9,13 +9,12 @@ class cron_puppet {
     }
     cron { 'puppet-pull':
         ensure  => present,
-        command => "cd /etc/puppet/code ; /usr/bin/git pull",
+        command => "cd /etc/puppet/code ; /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp",
         user    => root,
         minute  => '*/10',
-        require => File['post-hook'],
     }
     cron { 'puppet-apply':
-        ensure  => present,
+        ensure  => absent,
         command => "sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp",
         user    => root,
         minute  => '*/15',
