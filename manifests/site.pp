2febe3ea4b67ef047fba4a3160814fc7efcdebe0
MacFan4000 <paulfkeffer@gmail.com>
2020-07-21 09:09:25 -0400

    Add ufw to puppet (#8)
    * Add ufw module
    
    Signed-off-by: MacFan4000 <paulfkeffer@gmail.com>
    
    * Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,7 @@ node '100.node4.net.fosshost.org' {
   include users
   include cron_puppet
   include motd
+  include ufw
 }
 node '112.node1.net.fosshost.org' {
   include cron_puppet
@@ -10,10 +11,12 @@ node '112.node1.net.fosshost.org' {
   include role::webserver
   include motd
   include role::toolserver
+  include ufw
   include phabricator
 }
 node '113.node1.net.fosshost.org' {
   include cron_puppet
   include users
   include motd
+  include ufw
 }
