9a3dbd484c3b530484e882870c7a77ad13ddc9a9
MacFan4000 <paulfkeffer@gmail.com>
2020-07-21 09:33:01 -0400

    Revert "Add ufw to puppet (#8)" (#9)
    This reverts commit 2febe3ea4b67ef047fba4a3160814fc7efcdebe0.

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,6 @@ node '100.node4.net.fosshost.org' {
   include users
   include cron_puppet
   include motd
-  include ufw
 }
 node '112.node1.net.fosshost.org' {
   include cron_puppet
@@ -11,12 +10,10 @@ node '112.node1.net.fosshost.org' {
   include role::webserver
   include motd
   include role::toolserver
-  include ufw
   include phabricator
 }
 node '113.node1.net.fosshost.org' {
   include cron_puppet
   include users
   include motd
-  include ufw
 }
