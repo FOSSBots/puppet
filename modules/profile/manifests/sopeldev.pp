5603e16f8dd4bba7b60634ac2ed8cfa5e443f30a
RhinosF1 <rhinosf1@gmail.com>
2020-10-06 15:48:53 +0100

    Update sopeldev.pp

--- modules/profile/manifests/sopeldev.pp
+++ modules/profile/manifests/sopeldev.pp
@@ -8,10 +8,25 @@ class profile::sopeldev(
         owner   => root,
         group   => root,
     }
-  file { 'post-dev-hook':
+  file { 'post-dev-core-hook':
         ensure  => file,
-        path    => '/srv/sopelbots/devcode/.git/hooks/post-merge',
-        source  => 'puppet:///modules/profile/post-merge-dev',
+        path    => '/srv/sopelbots/devcode/core/.git/hooks/post-merge',
+        source  => 'puppet:///modules/profile/post-merge-dev-core',
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+    file { 'post-dev-adminlist-hook':
+        ensure  => file,
+        path    => '/srv/sopelbots/devcode/adminlist/.git/hooks/post-merge',
+        source  => 'puppet:///modules/profile/post-merge-dev-adminlist',
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+    file { 'post-dev-adminlist-hook':
+        ensure  => directory,
+        path    => '/srv/sopelbots/devcode/',
         mode    => '0755',
         owner   => root,
         group   => root,
