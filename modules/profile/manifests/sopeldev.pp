b93a182ef98e2a40af5f55de82fd89a58f775c5d
RhinosF1 <rhinosf1@gmail.com>
2020-10-06 17:03:32 +0100

    Revert "Revert "sopeldev: configure post-BigChange env (#16)" (#17)"
    This reverts commit 168db59a6f150343b9da3b90cd6457c06c538fec.

--- modules/profile/manifests/sopeldev.pp
+++ modules/profile/manifests/sopeldev.pp
@@ -8,17 +8,50 @@ class profile::sopeldev(
         owner   => root,
         group   => root,
     }
-  file { 'post-dev-hook':
+  file { 'post-dev-core-hook':
         ensure  => file,
-        path    => '/srv/sopelbots/devcode/.git/hooks/post-merge',
-        source  => 'puppet:///modules/profile/post-merge-dev',
+        path    => '/srv/sopelbots/devcode/core/.git/hooks/post-merge',
+        source  => 'puppet:///modules/profile/post-merge-dev-core',
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+    file { 'post-dev-adminlist-hook':
+        ensure  => file,
+        path    => '/srv/sopelbots/devcode/adminlist/.git/hooks/post-merge',
+        source  => 'puppet:///modules/profile/post-merge-dev-adminlist',
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+    file { 'post-dev-jsonparser-hook':
+        ensure  => file,
+        path    => '/srv/sopelbots/devcode/jsonparser/.git/hooks/post-merge',
+        source  => 'puppet:///modules/profile/post-merge-dev-jsonparser',
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+    file { 'post-dev-adminlist-hook':
+        ensure  => directory,
+        path    => '/srv/sopelbots/devcode/',
         mode    => '0755',
         owner   => root,
         group   => root,
     }
     git::clone { 'MirahezeBots/MirahezeBots':
         ensure    => 'latest',
-        directory => '/srv/sopelbots/devcode',
+        directory => '/srv/sopelbots/devcode/core',
+        branch    => 'dev',
+    }
+    git::clone { 'MirahezeBots/adminlist':
+        ensure    => 'latest',
+        directory => '/srv/sopelbots/devcode/adminlist',
+        branch    => 'dev',
+    }
+    git::clone { 'MirahezeBots/jsonparser':
+        ensure    => 'latest',
+        directory => '/srv/sopelbots/devcode/jsonparser',
         branch    => 'dev',
     }
 }
