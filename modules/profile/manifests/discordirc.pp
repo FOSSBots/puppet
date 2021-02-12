6b6740584aba2ee9da4f81d54aa5526dfd71c86c
MacFan4000 <paulfkeffer@gmail.com>
2021-02-12 17:08:21 -0500

    Puppetize discordirc (#55)
    * Create discordirc.pp
    
    * Create mhconfig.json
    
    * Create fhconfig.json
    
    * Update fhconfig.json
    
    * Update fhconfig.json
    
    * Update fhconfig.json

---
+++ modules/profile/manifests/discordirc.pp
@@ -0,0 +1,15 @@
+class profile::discordirc(){
+    $discordmh_token = lookup('passwords::discord::mh')
+    $discordmh_password = lookup('passwords::irc::mh')
+    $discordfh_token = lookup('passwords::discord::fh')
+    $discordfh_password = lookup('passwords::irc::fh')
+
+    file { '/discord-irc/mhconfig.json':
+        ensure  => present,
+        content => template('profile/mhconfig.json'),
+    }
+    
+    file { '/discord-irc/fhconfig.json':
+        ensure  => present,
+        content => template('profile/fhconfig.json'),
+    }
