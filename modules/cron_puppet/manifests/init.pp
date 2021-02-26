c24108d0539ccce91a3eb5b20569d9cb1e080237
MacFan4000 <paulfkeffer@gmail.com>
2021-02-26 09:40:41 -0500

    log to file

--- modules/cron_puppet/manifests/init.pp
+++ modules/cron_puppet/manifests/init.pp
@@ -1,7 +1,7 @@
 class cron_puppet {
     cron { 'puppet-pull':
         ensure  => present,
-        command => "cd /etc/puppet/code ; sudo /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp",
+        command => "cd /etc/puppet/code ; sudo /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp > /var/log/puppet.log",
         user    => root,
         minute  => '*/10',
     }
