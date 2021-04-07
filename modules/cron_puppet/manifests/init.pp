7a1a9b62b366e3c012fff92d3e91450d272aa62e
RhinosF1 <rhinosf1@gmail.com>
2021-04-07 11:38:37 +0100

    Update and rename modules/cron_puppet/manifests/init.pp to modules/puppet/manifests/init.pp

--- modules/cron_puppet/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class cron_puppet {
-    cron { 'puppet-pull':
-        ensure  => present,
-        command => "cd /etc/puppet/code ; sudo /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp > /var/log/puppet.log",
-        user    => root,
-        minute  => '*/10',
-    }
-}
