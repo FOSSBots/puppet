acbea22cee72b2fa2b600f1d05fea357d3405680
RhinosF1 <rhinosf1@gmail.com>
2021-04-07 12:02:06 +0100

    Revert "Update and rename modules/cron_puppet/manifests/init.pp to modules/puppet/manifests/init.pp (#99)"
    This reverts commit 027d4d22f9021da813f0f4cc5ca3d405b8e1039f.

--- modules/puppet/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class puppet {
-    systemd::timer::job { 'run-puppet':
-        ensure      => present,
-        description => 'Puppet 10 Minute run',
-        command => "cd /etc/puppet/code ; sudo /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp > /var/log/puppet.log",
-        user    => root,
-        interval    => {'start' => 'OnCalendar', 'interval' => '*:0/10'},
-    }
-}
