e43fea250888be01f4053421eff2062ffc5ad8f9
RhinosF1 <rhinosf1@gmail.com>
2021-04-07 12:01:43 +0100

    Revert "Update init.pp"
    This reverts commit 0f566aac27c61b118141d391dea2f7a82ba153ff.

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -2,7 +2,7 @@ class puppet {
     systemd::timer::job { 'run-puppet':
         ensure      => present,
         description => 'Puppet 10 Minute run',
-        command => "/usr/bin/cd /etc/puppet/code ; /usr/bin/git pull ; /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp > /var/log/puppet.log",
+        command => "cd /etc/puppet/code ; sudo /usr/bin/git pull ; sudo /usr/bin/puppet apply /etc/puppet/code/manifests/site.pp > /var/log/puppet.log",
         user    => root,
         interval    => {'start' => 'OnCalendar', 'interval' => '10min'},
     }
