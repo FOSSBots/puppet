53a81958e6791f9b6eeaec566e0eb856aaaf6af9
RhinosF1 <rhinosf1@gmail.com>
2021-04-07 13:27:30 +0100

    Update init.pp

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -2,7 +2,7 @@ class puppet {
     systemd::timer::job { 'run-puppet':
         ensure      => present,
         description => 'Puppet 10 Minute run',
-        command     => "puppet-run",
+        command     => "/usr/bin/puppet-run",
         user        => root,
         interval    => {'start' => 'OnCalendar', 'interval' => '*-*-* *:00/10:00'},
     }
