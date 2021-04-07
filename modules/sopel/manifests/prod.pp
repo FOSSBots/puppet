ef129d348c1f245e7f2db89baeed3af89e72f73b
RhinosF1 <rhinosf1@gmail.com>
2021-04-07 14:06:50 +0100

    Create prod.pp

---
+++ modules/sopel/manifests/prod.pp
@@ -0,0 +1,30 @@
+# == Class sopelshared
+#
+# Installs the shared stuff between both sopel instances.
+#
+class sopel(
+){
+  file { 'prod-venv':
+        ensure  => directory,
+        path    => '/srv/sopelbots/prodvenv/',
+        mode    => '770',
+        owner   => sopel,
+        group   => sopel,
+    }
+    file { 'prod-require':
+        ensure  => file,
+        path    => '/srv/sopelbots/prodrequire.txt',
+        source  => 'puppet:///modules/sopel/prodrequire.txt',
+        mode    => '770',
+        owner   => sopel,
+        group   => sopel,
+        notify  => Exec['update prod'],
+    }
+    exec { 'update prod':
+          command     => '/srv/sopelbots/prodvenv/bin/pip3.7 install -U -r /srv/sopelbots/prodrequire.txt',
+          cwd         => '/srv/sopelbots/prodvenv',
+          refreshonly => true,
+          user        => sopel,
+          require     => File['prod-venv', 'prod-require']
+    }
+    }
