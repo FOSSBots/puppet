1e98589092df6e4b7dcd24236ae4ceaa4d242ba5
RhinosF1 <rhinosf1@gmail.com>
2021-04-08 13:57:16 +0100

    enable monitoring on phabdigests

--- modules/profile/manifests/phabdigest.pp
+++ modules/profile/manifests/phabdigest.pp
@@ -13,6 +13,7 @@ class profile::phabdigest(
         description => 'Weekly PhabDigest for botsphab',
         command     => "/usr/bin/python3 /srv/phabdigest/script.py /srv/phabdigest/weekly.csv bots",
         user        => root,
+        monitoring_enabled => true,
         interval    => {'start' => 'OnCalendar', 'interval' => 'Mon *-*-* 15:00:00'},
     }
 }
