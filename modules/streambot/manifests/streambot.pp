49fac556049de8b4d2e5f0f9dd01a3e28e693dda
MacFan4000 <paulfkeffer@gmail.com>
2021-04-14 14:10:24 -0400

    add streambot to puppet (#115)
    * Create streambot.pp
    
    * Create settings.py
    
    * Create streambot.systemd.erb
    
    * Update botserver.pp
    
    * Update streambot.systemd.erb
    
    * Update streambot.pp
    
    * Update streambot.pp
    
    * Update and rename modules/profile/manifests/streambot.pp to modules/streambot/manifests/streambot.pp
    
    * Rename modules/profile/templates/initscripts/streambot.systemd.erb to modules/streambot/templates/initscripts/streambot.systemd.erb
    
    * Rename modules/profile/templates/settings.py to modules/streambot/templates/settings.py
    
    * Update botserver.pp
    
    * Update streambot.pp

---
+++ modules/streambot/manifests/streambot.pp
@@ -0,0 +1,24 @@
+class streambot{
+    $streambot_pass = lookup('passwords::streambot::irc')
+    git::clone { 'MirahezeBots/snitchbot':
+        ensure    => 'latest',
+        directory => '/srv/streambot',
+        branch    => 'master',
+        owner     => streambot,
+        group     => streambot,
+        recurse_submodules => true,
+    }
+    systemd::service { 'streambot':
+        ensure  => present,
+        content => systemd_template('streambot'),
+        restart => true,
+    }
+    file { '/srv/streambot/settings.py':
+        ensure  => present,
+        content => template('streambot/settings.py'),
+        notify  => Service['streambot'],
+        mode    => '770',
+        owner   => streambot,
+        group   => streambot,
+    }
+}
---
+++ modules/streambot/templates/settings.py
@@ -0,0 +1,10 @@
+database = '/srv/streambot/snitch.db'
+
+snatch_network = 'irc.freenode.net'
+snitch_network = 'irc.freenode.net'
+
+nickserv_password = '<%= @streambot_pass %>'
+
+nickname = 'StreamBotMH'
+
+authorized_users = ('miraheze/RhinosF1', 'wikimedia/MacFan4000', 'miraheze/Reception123', 'wikipedia/The-Voidwalker', 'quirc/admin')
