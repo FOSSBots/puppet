bf5b0ee76500d5ae360fffed2e8e86f05725e375
MacFan4000 <paulfkeffer@gmail.com>
2021-05-19 15:00:33 -0400

    Create mhliberaconfig.json (#145)
    * Create mhliberaconfig.json
    
    * Create mhdiscordlibera.systemd.erb
    
    * Update mhliberaconfig.json
    
    * Update discordirc.pp

--- modules/profile/manifests/discordirc.pp
+++ modules/profile/manifests/discordirc.pp
@@ -1,6 +1,7 @@
 class profile::discordirc{
     $discordmh_token = lookup('passwords::discord::mh')
     $discordmh_password = lookup('passwords::irc::mh')
+    $discordmhlibera_password = lookup('passwords::irc::mhlibera')
     $discordfh_token = lookup('passwords::discord::fh')
     $discordfh_password = lookup('passwords::irc::fh')
     systemd::service { 'discordircmh':
@@ -15,10 +16,25 @@ class profile::discordirc{
         restart => true,
         require => File['/discord-irc/fhconfig.json'],
     }
+    systemd::service { 'mhdiscordlibera':
+        ensure  => present,
+        content => systemd_template('mhdiscordlibera'),
+        restart => true,
+        require => File['/discord-irc/mhconfiglibera.json'],
+    }
     file { '/discord-irc/mhconfig.json':
         ensure  => present,
         content => template('profile/mhconfig.json'),
         notify  => Service['discordircmh'],
+        mode    => '770',
+        owner   => relays,
+        group   => relays,
+    }
+    
+    file { '/discord-irc/mhconfiglibera.json':
+        ensure  => present,
+        content => template('profile/mhconfiglibera.json'),
+        notify  => Service['mhdiscordlibera'],
         mode    => '770',
         owner   => relays,
         group   => relays,
