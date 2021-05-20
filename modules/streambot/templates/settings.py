7528241e9e3e5efb7fa97b2a20ad3e9ea9e4cfe2
MacFan4000 <paulfkeffer@gmail.com>
2021-05-20 12:37:05 -0400

    freenode -> libera and wikimedia/wikipedia/miraheze -> user

--- modules/streambot/templates/settings.py
+++ modules/streambot/templates/settings.py
@@ -1,10 +1,10 @@
 database = '/srv/streambot/snitch.db'
 
-snatch_network = 'irc.freenode.net'
-snitch_network = 'irc.freenode.net'
+snatch_network = 'irc.libera.chat'
+snitch_network = 'irc.libera.chat'
 
 nickserv_password = '<%= @streambot_pass %>'
 
 nickname = 'StreamBotMH'
 
-authorized_users = ('miraheze/RhinosF1', 'wikimedia/MacFan4000', 'miraheze/Reception123', 'wikipedia/The-Voidwalker', 'miraheze/Sario528')
+authorized_users = ('user/RhinosF1', 'user/MacFan4000', 'user/Reception123', 'user/The-Voidwalker', 'user/Sario528')
