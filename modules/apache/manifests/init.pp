9ea26d81a9dee206b3b4a169b5da2daeb74e60ea
RhinosF1 <rhinosf1@gmail.com>
2021-05-30 16:51:58 +0100

    Update init.pp

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -9,7 +9,7 @@ class apache {
     }
 
     file { '/etc/apache2/sites-available/mirahezebots.org-le-ssl.conf':
-        ensure  => file,
+        ensure  => present,
         path    => '/etc/apache2/sites-available/mirahezebots.org-le-ssl.conf',
         source  => 'puppet:///modules/apache/mirahezebots.org-le-ssl.conf',
         mode    => '774',
@@ -18,7 +18,7 @@ class apache {
     }
 
     file { '/etc/apache2/sites-available/www.mirahezebots.org-le-ssl.conf':
-        ensure  => file,
+        ensure  => present,
         path    => '/etc/apache2/sites-available/www.mirahezebots.org-le-ssl.conf',
         source  => 'puppet:///modules/apache/www.mirahezebots.org-le-ssl.conf',
         mode    => '774',
@@ -29,7 +29,7 @@ class apache {
     if $trusted['hostname'] == 'bots1.miraheaebots.org' {
 
         file { '/etc/apache2/sites-available/bots1.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/bots1.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/bots1.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -38,7 +38,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/sopel.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/sopel.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/sopel.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -47,7 +47,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/100.node4.net.fosshost.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/100.node4.net.fosshost.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/100.node4.net.fosshost.org-le-ssl.conf',
             mode    => '774',
@@ -59,7 +59,7 @@ class apache {
     if $trusted['hostname'] == 'tools1.miraheaebots.org' {
 
         file { '/etc/apache2/sites-available/icinga.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/icinga.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/icinga.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -68,7 +68,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/phabdigests.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/phabdigests.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/phabdigests.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -77,7 +77,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/phab.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/phab.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/phab.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -86,7 +86,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/phab-storage.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/phab-storage.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/phab-storage.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -95,7 +95,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/tools1.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/tools1.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/tools1.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -104,7 +104,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/webmail.mirahezebots.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/webmail.mirahezebots.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/webmail.mirahezebots.org-le-ssl.conf',
             mode    => '774',
@@ -113,7 +113,7 @@ class apache {
         }
 
         file { '/etc/apache2/sites-available/112.node1.net.fosshost.org-le-ssl.conf':
-            ensure  => file,
+            ensure  => present,
             path    => '/etc/apache2/sites-available/112.node1.net.fosshost.org-le-ssl.conf',
             source  => 'puppet:///modules/apache/112.node1.net.fosshost.org-le-ssl.conf',
             mode    => '774',
@@ -123,7 +123,7 @@ class apache {
     }
 
     file { '/etc/apache2/apache2.conf':
-        ensure  => file,
+        ensure  => present,
         path    => '/etc/apache2/apache2.conf',
         source  => 'puppet:///modules/apache/apache2.conf',
         mode    => '774',
