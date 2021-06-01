18c682f67c58a2a10bd7994e63211072ba79a4e3
RhinosF1 <rhinosf1@gmail.com>
2021-06-01 14:10:50 +0100

    Update discordirc.pp

--- modules/profile/manifests/discordirc.pp
+++ modules/profile/manifests/discordirc.pp
@@ -2,6 +2,7 @@ class profile::discordirc{
     $discordmh_token = lookup('passwords::discord::mh')
     $discordmhlibera_password = lookup('passwords::irc::mhlibera')
     $discordfh_token = lookup('passwords::discord::fh')
+    $discordfhfn_token = lookup('passwords::discord::fhfn')
     $discordfh_password = lookup('passwords::irc::fh')
     $discordfhlibera_password = lookup('passwords::irc::fhlibera')
     $discordbuff_token = lookup('passwords::discord::buff')
