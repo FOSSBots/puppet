597a1d4a788dc2e7a60c684dffecc282a5bf1877
MacFan4000 <paulfkeffer@gmail.com>
2021-06-08 11:02:08 -0400

    Update master.pp

--- modules/icinga2/manifests/master.pp
+++ modules/icinga2/manifests/master.pp
@@ -10,7 +10,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/conf.d/commands.conf':
         ensure  => present,
-        content => template('icinga/commands.conf'),
+        content => template('icinga2/commands.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -18,7 +18,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/conf.d/notifications.conf':
         ensure  => present,
-        content => template('icinga/notifications.conf'),
+        content => template('icinga2/notifications.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -26,7 +26,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/conf.d/services.conf':
         ensure  => present,
-        content => template('icinga/services.conf'),
+        content => template('icinga2/services.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -34,7 +34,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/conf.d/templates.conf':
         ensure  => present,
-        content => template('icinga/templates.conf'),
+        content => template('icinga2/templates.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -42,7 +42,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/conf.d/users.conf':
         ensure  => present,
-        content => template('icinga/users.conf'),
+        content => template('icinga2/users.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -50,7 +50,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/zones.conf':
         ensure  => present,
-        content => template('icinga/master-zones.conf'),
+        content => template('icinga2/master-zones.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -58,7 +58,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/zones.d/master/services.conf':
         ensure  => present,
-        content => template('icinga/zones.d/master/services.conf'),
+        content => template('icinga2/zones.d/master/services.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -66,7 +66,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/zones.d/master/hosts.conf':
         ensure  => present,
-        content => template('icinga/zones.d/master/hosts.conf'),
+        content => template('icinga2/zones.d/master/hosts.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
@@ -74,7 +74,7 @@ class icinga2::master{
     
     file { '/etc/icinga2/zones.d/global-templates/commands.conf':
         ensure  => present,
-        content => template('icinga/zones.d/global-templates/commands.conf'),
+        content => template('icinga2/zones.d/global-templates/commands.conf'),
         mode    => '775',
         owner   => root,
         group   => root,
