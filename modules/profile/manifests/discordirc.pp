e2dd0c9374cd01e7360d610492924c936c4d8637
MacFan4000 <paulfkeffer@gmail.com>
2022-02-21 21:03:52 -0500

    remove old relays

--- modules/profile/manifests/discordirc.pp
+++ modules/profile/manifests/discordirc.pp
@@ -2,7 +2,6 @@ class profile::discordirc(){
     $discordmh_token = lookup('passwords::discord::mh')
     $discordmhlibera_password = lookup('passwords::irc::mhlibera')
     $discordfh_token = lookup('passwords::discord::fh')
-    $discordfhlibera_password = lookup('passwords::irc::fhlibera')
     $discordbuff_token = lookup('passwords::discord::buff')
     $discordballmedia_token = lookup('passwords::discord::ballmedia')
     $zulipphorge_token = lookup('passwords::zulip::phorge')
@@ -16,28 +15,6 @@ class profile::discordirc(){
         shell      => '/bin/sh',
     }
 
-    $relays = ['fhlibera', 'mhlibera', 'buff', 'ballmedia']
-    $relays.each |$relay| {
-    $description = "Discord-IRC (${relay})"
-    $exec = "npm start -- --config /discord-irc/${relay}config.json"
-
-    systemd::service { "discordirc${relay}":
-        ensure  => present,
-        content => systemd_template("discordirc"),
-        restart => true,
-        require => [File["/discord-irc/${relay}config.json"], User['relays']],
-    }
-
-    file { "/discord-irc/${relay}config.json":
-        ensure  => present,
-        content => template("profile/${relay}config.json"),
-        notify  => Service["discordirc${relay}"],
-        mode    => '770',
-        owner   => relays,
-        group   => relays,
-        require => User['relays']
-    }
-}
     file { "/srv/matterbridge/matterbridge.toml":
         ensure  => present,
         content => template("profile/matterbridge.toml"),
