c53c00845e8546d048ab9890bb660eca48db3b64
RhinosF1 <rhinosf1@gmail.com>
2022-05-08 16:54:13 +0100

    dbbackups: add

---
+++ modules/profile/manifests/dbbackups.pp
@@ -0,0 +1,17 @@
+class dbbackups {
+    systemd::timer::job { 'backup-db':
+        ensure      => present,
+        description => 'Backup Database',
+        command     => "/usr/bin/backup-db,
+        user        => root,
+        interval    => {'start' => 'OnCalendar', 'interval' => '*/7 *-*-* *:*:*'},
+    }
+    file { 'db-backup-script':
+        ensure  => file,
+        path    => '/usr/bin/backup-db',
+        source  => 'puppet:///modules/profile/backup-db.sh',
+        mode    => '777',
+        owner   => root,
+        group   => root,
+    }
+}
