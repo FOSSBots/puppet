83cb898681347da73803c415277478d4a52ff574
RhinosF1 <rhinosf1@gmail.com>
2022-05-15 15:51:50 +0100

    backups: add phab

---
+++ modules/profile/files/backup-phab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo -u www-data /var/phab-deploy/phabricator/bin/phd stop
+cp -r /var/phab /backups/phab
+tar -czvf /backups/phab.tar.gz /backups/phab
+s3cmd -c /backups/.s3cfg sync /backups/phab.tar.gz s3://backups-fossbots
