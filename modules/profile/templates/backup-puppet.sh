8382d992ac27fd66f42e2fca9ebd9c5fbcde7401
RhinosF1 <rhinosf1@gmail.com>
2022-05-16 16:34:37 +0100

    Update backup-puppet.sh

--- modules/profile/templates/backup-puppet.sh
+++ modules/profile/templates/backup-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /etc/puppet/private/hieradata/common.yaml /backups/<%= $facts[@fqdn] %>.yaml
-s3cmd -c /backups/.s3cfg /backups/<%= $facts[@fqdn] %>.yaml s3://backup-fossbots
+cp /etc/puppet/private/hieradata/common.yaml /backups/<%= $facts['fqdn'] %>.yaml
+s3cmd -c /backups/.s3cfg /backups/<%=  $facts['fqdn'] %>.yaml s3://backup-fossbots
