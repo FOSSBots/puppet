6e3407941cb091307e0775d558f36d3753b3c2da
RhinosF1 <rhinosf1@gmail.com>
2022-05-16 16:49:46 +0100

    Update backup-puppet.sh

--- modules/profile/templates/backup-puppet.sh
+++ modules/profile/templates/backup-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp /etc/puppet/private/hieradata/common.yaml /backups/<%= @hostname %>.yaml
-s3cmd -c /backups/.s3cfg /backups/<%=  @hostname %>.yaml s3://backup-fossbots
+s3cmd -c /backups/.s3cfg sync /backups/<%=  @hostname %>.yaml s3://backup-fossbots
