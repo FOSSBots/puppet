a486275c819a1cd7e9a5c45be07e72b8fa9aa5e1
MacFan4000 <paulfkeffer@gmail.com>
2022-06-01 12:14:53 -0400

    add mariadb config to puppet

---
+++ modules/mariadb/manifests/init.pp
@@ -0,0 +1,19 @@
+class apache {
+    package { 'mariadb-server':
+        ensure => present,
+    }
+
+    service { 'mysql':
+        ensure  => running,
+        require => Package['mariadb-server'],
+    }
+
+    file { '/etc/mysql/mariadb.conf.d/50-server.cnf':
+        ensure  => present,
+        path    => '/etc/mysql/mariadb.conf.d/50-server.cnf',
+        source  => 'puppet:///modules/mariadb/50-server.cnf',
+        mode    => '774',
+        owner   => root,
+        group   => root,
+    }
+}
