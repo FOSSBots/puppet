084240aa760e70510c6a0f0542b6da06f1aedb1e
RhinosF1 <rhinosf1@gmail.com>
2022-08-05 17:13:05 +0100

    stop applying icinga:master to t3001
    doesn't work

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,7 +13,6 @@ node 'bots3001.fossbots.org' {
 }
 node 'tools3001.fossbots.org' {
   include role::toolserver
-  include ::icinga2::master
 }
 node 'db3001.fossbots.org' {
   include role::dbserver
