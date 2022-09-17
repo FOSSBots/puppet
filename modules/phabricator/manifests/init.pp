9eb2bdcba6cfb10c2648b1bab65f284a7f595e55
RhinosF1 <rhinosf1@gmail.com>
2022-09-17 13:58:35 +0100

    Update init.pp

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -66,7 +66,7 @@ class phabricator {
             {
                  'host'  => 'db2002.fossbots.org',
                  'role' => 'master',
-                 'disabled' => false,
+                 'disabled' => true,
             },
             {
                  'host'  => 'db3001.fossbots.org',
