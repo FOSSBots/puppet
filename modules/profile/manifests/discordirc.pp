d7f4d5bca157784b1ab17b9e69dd99afe741269d
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-08 01:20:55 -0700

    Remove Miraheze relay channels (#425)
    * Remove Miraheze relay channels
    
    * Update discordirc.pp

--- modules/profile/manifests/discordirc.pp
+++ modules/profile/manifests/discordirc.pp
@@ -1,6 +1,4 @@
 class profile::discordirc(){
-    $discordmh_token = lookup('passwords::discord::mh')
-    $discordmhprivate_token = lookup('passwords::discord::mhprivate')
     $discordmhlibera_password = lookup('passwords::irc::mhlibera')
     $discordfh_token = lookup('passwords::discord::fh')
     $discordbuff_token = lookup('passwords::discord::buff')
