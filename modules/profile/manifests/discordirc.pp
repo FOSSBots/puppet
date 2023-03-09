787967aec893b49af6032f3a12fb0707d3283446
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-08 21:40:04 -0700

    Update discordirc.pp

--- modules/profile/manifests/discordirc.pp
+++ modules/profile/manifests/discordirc.pp
@@ -2,7 +2,6 @@ class profile::discordirc(){
     $discordmhlibera_password = lookup('passwords::irc::mhlibera')
     $discordfh_token = lookup('passwords::discord::fh')
     $discordbuff_token = lookup('passwords::discord::buff')
-    $discordballmedia_token = lookup('passwords::discord::ballmedia')
     $zulipphorge_token = lookup('passwords::zulip::phorge')
     $ensure = lookup('bridgebot::timer')
 
